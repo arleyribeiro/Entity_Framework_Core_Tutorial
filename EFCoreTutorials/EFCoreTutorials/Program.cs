df13048250fb24f19a327a57477e707066d00cf6
arleyribeiro <arley.sribeiro@gmail.com>
2019-02-01 10:56:01 -0200

    Initial commit and models basic with first migration

---
+++ EFCoreTutorials/EFCoreTutorials/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EFCoreTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                var std = new Student()
+                {
+                    Name = "Arley"
+                };
+
+                context.Students.Add(std);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ EFCoreTutorials/EFCoreTutorials/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+
+namespace EFCoreTutorials
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(@"server=localhost;userid=root;password=root;database=schooldb",  builder => builder.MigrationsAssembly("EFCoreTutorials"));
+        }
+    }
+}
